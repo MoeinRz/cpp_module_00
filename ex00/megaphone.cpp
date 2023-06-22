5665a7876e8fa2e17e5f4c084c7944bd0c7ea1cf
mrezaei <moeinrezaei330@gmail.com>
2023-06-22 20:17:48 +0200

    ipdated.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -9,3 +9,75 @@
 /*   Updated: 2023/12/22 20:02:09 by moeinrz                                  */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc <= 1)
+		std::cout << "No argument provided." << std::endl;
+	else
+	{
+		std::cout << "You entered: " << argv[1] << std::endl;
+	}
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+// #include <cctype>
+// #include <string>
+
+// int	main(int argc, char **argv) {
+
+
+// 	if (argc == 1)
+// 	{
+// 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+// 		return 0;
+// 	}
+
+// 	std::string	arg;
+// 	std::string::const_iterator it;
+	
+// 	int	i = 1;
+// 	while (argv[i] != NULL)
+// 	{
+// 		arg = argv[i];
+// 		for (it = arg.begin() ; it != arg.end() ; ++it) {
+// 			std::cout << (char) std::toupper((int) *it);
+// 		}
+// 		i++;
+// 	}
+// 	std::cout << std::endl;
+
+// 	return 0;
+// }
